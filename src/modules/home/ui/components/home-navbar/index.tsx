3e412c09c34feb80f7fb3505c617cbd1e2b52bd5
IAmShaaann <iamshaaann@gmail.com>
2025-03-02 19:38:19 +0530

    chapter two - home navigation bar search component

---
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -0,0 +1,33 @@
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { SearchInput } from "@/modules/home/ui/components/home-navbar/search-input";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export const HomeNavbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
+      <div className="flex items-center gap-4 w-full">
+        {/* Menu and Logo */}
+        <div className="flex items-center  flex-shrink-0">
+          <SidebarTrigger />
+          <Link href={"/"}>
+            <div className="p-4 flex items-center gap-1 ">
+            <Image
+              src={"/logo.svg"}
+              height={32}
+              width={32}
+              alt="tuberrr-logo"
+            />
+            <p className="text-xl font-semibold tracking-tight">Tuberrr</p>
+            </div>
+          </Link>
+        </div>
+        {/* Search bar */}
+        <div className="flex-1 flex justify-center max-w-[720px] mx-auto">  
+            <SearchInput/>
+        </div>
+      </div>
+    </nav>
+  );
+};
